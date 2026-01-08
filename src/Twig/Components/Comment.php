d173a92b7c44a4c782d64a6856c12a0b701b12fe
d9beuD <53569029+d9beuD@users.noreply.github.com>
2026-01-08 16:06:46 +0100

    Add Comment Twig component

---
+++ src/Twig/Components/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\Comment as EntityComment;
+use App\Entity\User;
+use App\Repository\CommentUpvoteRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class Comment
+{
+    use DefaultActionTrait;
+
+    public EntityComment $comment;
+
+    public function __construct(
+        protected CommentUpvoteRepository $commentUpvoteRepository,
+        protected Security $security,
+    ) {}
+
+    public function countUpvotes(): int
+    {
+        return $this->commentUpvoteRepository->count([
+            'comment' => $this->comment,
+            'user' => $this->security->getUser(),
+        ]);
+    }
+}
